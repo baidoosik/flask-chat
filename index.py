088adf375e128edf2ae697e2b3b20fc792c0f002
baidoosik <qoentlr37@naver.com>
2017-11-02 15:29:08 +0900

    flask-chat 예제 간단 구현

---
+++ index.py
@@ -0,0 +1,57 @@
+import os
+from flask import Flask, render_template, session
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.secret_key = 'hard'
+app.debug=True
+socketio = SocketIO(app)
+
+user_no = 1
+
+
+@app.before_request
+def before_request():
+    global user_no
+    if 'session' in session and 'user-id' in session:
+        pass
+    else:
+        session['session'] = os.urandom(24)
+        session['username'] = 'user' + str(user_no)
+        user_no +=1
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('connect', namespace='/mynamespace')
+def connect():
+    emit("response", {
+        'data': 'conected',
+        'username': session['username']
+    })
+
+
+@socketio.on('disconnect', namespace='/mynamespace')
+def disconnect():
+    session.clear()
+    print("disconnected")
+
+
+@socketio.on("request", namespace='/mynamespace')
+def request(message):
+    emit("response", {
+        'data': message['data'],
+        'username': session['username']
+    }, broadcast=True)
+
+
+@socketio.on('onClose', namespace='/mynamespace')
+def disconnect():
+    session.clear()
+    print("disconnected")
+
+if __name__ == '__main__':
+    socketio.run(app)
